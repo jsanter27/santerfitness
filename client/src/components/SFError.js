148cd9747f2040b87036fcb426634f65fc307fda
jsanter27 <jsanter@cs.stonybrook.edu>
2020-07-29 13:24:27 -0400

    Added chmod to buildspec.yml

--- client/src/components/SFError.js
+++ client/src/components/SFError.js
@@ -5,7 +5,7 @@ const SFError = (props) => {
     return (
         <Container>
             <Row className="my-auto d-flex justify-content-center">
-                <h2 className="sf-loading-text">{props.error ? props.error.message : "An unexpected error has occurred"}</h2>
+                <h2 className="sf-loading-text">An unexpected error has occurred</h2>
             </Row>
             <Row className="my-auto d-flex justify-content-center">
                 <h4 className="sf-loading-text">Please check back later</h4>
