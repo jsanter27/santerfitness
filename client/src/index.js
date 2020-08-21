b83566f36db87ff5fb930fb40623f73bd6084fea
jsanter27 <jsanter@cs.stonybrook.edu>
2020-08-21 18:01:22 -0400

    Changed package.json homepage

--- client/src/index.js
+++ client/src/index.js
@@ -20,9 +20,9 @@ import AdminForgotPasswordScreen from './components/AdminForgotPasswordScreen';
 import AdminChangePasswordScreen from './components/AdminChangePasswordScreen';
 
 
-//const client = new ApolloClient({ uri: "http://localhost:" + (process.env.PORT || "5000") + "/graphql" });
+const client = new ApolloClient({ uri: "http://localhost:" + (process.env.PORT || "5000") + "/graphql" });
 
-const client = new ApolloClient({ uri: "http://prod.eba-pwbm3ckm.us-east-2.elasticbeanstalk.com/graphql"});
+//const client = new ApolloClient({ uri: "http://prod.eba-pwbm3ckm.us-east-2.elasticbeanstalk.com/graphql"});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
