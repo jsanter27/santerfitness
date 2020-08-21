8565964639980546437c4841034cfe67facc7618
jsanter27 <jsanter@cs.stonybrook.edu>
2020-08-21 17:36:58 -0400

    Adjusted SFModal messaging

--- client/src/components/SFModal.js
+++ client/src/components/SFModal.js
@@ -4,19 +4,35 @@ import { DARK } from '../constants/Colors';
 
 const SFModal = (props) => {
 
-    return (
-        <Modal show={props.show} onHide={props.onHide} className="sf-modal">
-            <Modal.Header className="sf-modal-header" closeButton>
-                <Modal.Title className="sf-modal-title" style={{color:(props.message.msgError ? "red" : DARK)}}>
-                    <b>{props.message.msgError ? "Error" : ""}</b>
-                    <b>{!props.message.msgError && props.message.msgBody.trim() !== '' ? "Success" : ""}</b>
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="sf-modal-body">
-                    {props.message.msgBody}
-            </Modal.Body>
-        </Modal>
-    );
+    if (props.message) {
+        return (
+            <Modal show={props.show} onHide={props.onHide} className="sf-modal">
+                <Modal.Header className="sf-modal-header" closeButton>
+                    <Modal.Title className="sf-modal-title" style={{color:(props.message.msgError ? "red" : DARK)}}>
+                        <b>{props.message.msgError ? "Error" : ""}</b>
+                        <b>{!props.message.msgError && props.message.msgBody.trim() !== '' ? "Success" : ""}</b>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="sf-modal-body">
+                        {props.message.msgBody}
+                </Modal.Body>
+            </Modal>
+        );
+    }
+    else {
+        return (
+            <Modal show={props.show} onHide={props.onHide} className="sf-modal">
+                <Modal.Header className="sf-modal-header" closeButton>
+                    <Modal.Title className="sf-modal-title" style={{color:DARK}}>
+                        <b>Complete</b>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="sf-modal-body">
+                        Please verify that the action has completed
+                </Modal.Body>
+            </Modal>
+        );
+    }
 };
 
 export default SFModal;
