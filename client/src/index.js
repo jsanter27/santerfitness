15b42cc63efc1488f2b33678b5a2fa8b2ab89111
jsanter27 <jsanter@cs.stonybrook.edu>
2020-08-21 21:47:22 -0400

    Site Version 1.0.2

--- client/src/index.js
+++ client/src/index.js
@@ -22,7 +22,7 @@ import AdminChangePasswordScreen from './components/AdminChangePasswordScreen';
 
 //const client = new ApolloClient({ uri: "http://localhost:" + (process.env.PORT || "5000") + "/graphql" });
 
-const client = new ApolloClient({ uri: "http:santerfitness.com/graphql"});
+const client = new ApolloClient({ uri: "http://santerfitness.com/graphql"});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
